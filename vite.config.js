8dd258eff0ec819694fffc15793275f9c5b9c816
Ethan D'Costa <ethanmdcosta@gmail.com>
2025-09-04 12:52:27 +0530

    updated package.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/EthanDCosta-Portfolio/",
+  base: "/ethan-dcosta-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
